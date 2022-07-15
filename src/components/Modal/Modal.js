c76479e9f6d879df943a4f6cde477f05626fa2b1
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-16 01:25:07 +0300

    Create Modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+export default function Modal(props) {
+  return (
+    <div className="lb-root">
+      <div className="lb-block">
+        <div className="head-item">
+          <div className="close-item">
+            <FontAwesomeIcon
+              icon={faXmark}
+              onClick={() => props.setOpenModal(false)}
+            />
+          </div>
+        </div>
+        <div className="img-item">
+          <img src={props.url} alt={props.id} />
+        </div>
+      </div>
+    </div>
+  );
+}
