eff1d8536816763fb296942973f0c261e3cf6b0b
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-15 19:57:22 +0300

    Create react router structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./style/main.css";
+
+import Home from "./pages/Home";
+import PictureDetail from "./pages/PictureDetail";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import Header from "./components/Header/Header";
+
+import DefaultLayout from "./layout/DefaultLayout";
+
+import * as ROUTES from "./constants/routePath";
 
 function App() {
-  return <div className="App">Humor Pictures</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path={ROUTES.HOME}
+          element={
+            <DefaultLayout>
+              <Home />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path={ROUTES.PICTUREDETAIL}
+          element={
+            <DefaultLayout>
+              <PictureDetail />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path={ROUTES.NOTFOUNDPAGE}
+          element={
+            <DefaultLayout>
+              <NotFoundPage />
+            </DefaultLayout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/constants/routePath.js
@@ -0,0 +1,6 @@
+export const HOME = "/";
+export const PICTUREDETAIL = "/picture/:id";
+
+export const PICTUREDETAIL_WITHOUT_PARAM = "/picture";
+
+export const NOTFOUNDPAGE = "*";
