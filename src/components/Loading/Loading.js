532e8c690d4dac234d85fe384570aaf97f193f88
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-16 11:13:50 +0300

    Update property name for loading area

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div class="stage">
-      <div class="dot-pulse"></div>
+    <div className="stage">
+      <div className="dot-pulse"></div>
     </div>
   );
 }
