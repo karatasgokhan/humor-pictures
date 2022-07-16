45821870cafbd9c5ea52ddd52a114651a2c7fa44
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-16 11:21:37 +0300

    Update route path file

---
+++ src/constants/dist/routePath.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.NOTFOUNDPAGE = exports.HOME = void 0;
+var HOME = "/";
+exports.HOME = HOME;
+var NOTFOUNDPAGE = "*";
+exports.NOTFOUNDPAGE = NOTFOUNDPAGE;
--- src/constants/routePath.js
+++ src/constants/routePath.js
@@ -1,6 +1,2 @@
 export const HOME = "/";
-export const PICTUREDETAIL = "/picture/:id";
-
-export const PICTUREDETAIL_WITHOUT_PARAM = "/picture";
-
 export const NOTFOUNDPAGE = "*";
