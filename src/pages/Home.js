78ee3e44297179ac28cd724afad4013121e46138
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-16 11:12:16 +0300

    Add infinite scroll

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,78 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import Modal from "../components/Modal/Modal";
 
-import DataTable from "react-data-table-component";
+import useInfiniteScroll from "../helper/useInfiniteScroll";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 import { useGetPicturesApiQuery } from "../store/apis/HumorPicturesApi";
-
-import * as ROUTES from "../constants/routePath";
+import Loading from "../components/Loading/Loading";
 
 export default function Home() {
-  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const [cellIndex, setCellIndex] = useState(-1);
+
   const { data } = useGetPicturesApiQuery({
     refetchOnMountOrArgChange: true,
   });
 
-  const columns = [
-    {
-      name: "",
-      cell: (row) => (
-        <div className="cell-root" onClick={() => onClick(row.id)}>
-          <div className="img-item">
-            <img src={row.url} alt={row.id} />
-          </div>
-          <p className="text">{row.name}</p>
-        </div>
-      ),
-    },
-  ];
+  const halfDataLength = data?.data?.memes?.length / 2;
+  const allDataLength = data?.data?.memes?.length;
+  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreListItems);
 
-  const onClick = (id) => {
-    navigate(`${ROUTES.PICTUREDETAIL_WITHOUT_PARAM}/${id}`);
-  };
+  const [listItems, setListItems] = useState(
+    data?.data?.memes.slice(0, halfDataLength)
+  );
+
+  function fetchMoreListItems() {
+    if (listItems.length >= data?.data?.memes?.length) {
+      setIsFetching(false);
+    } else {
+      setTimeout(() => {
+        setListItems((prevState) => [
+          ...prevState,
+          ...data?.data?.memes.slice(halfDataLength, allDataLength),
+        ]);
+        setIsFetching(false);
+      }, 2000);
+    }
+  }
 
   return (
-    <DataTable
-      columns={columns}
-      data={data?.data?.memes ? data?.data?.memes : []}
-      highlightOnHover
-    />
+    <>
+      <ul className="table-root">
+        {listItems.map((listItem, index) => (
+          <div
+            key={index}
+            className="cell-root"
+            onClick={() => {
+              setCellIndex(index);
+              setIsOpen(true);
+            }}
+          >
+            <div className="cell-wrapper">
+              <div className="img-item">
+                <img src={listItem.url} alt={listItem.id} />
+              </div>
+              <div className="text-item">
+                <p>{listItem.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </ul>
+      <div className="scroll-item" onClick={() => window.scrollTo(0, 0)}>
+        <FontAwesomeIcon icon={faCircleArrowUp} />
+      </div>
+      {isFetching && <Loading />}
+      {isOpen && (
+        <Modal
+          setIsOpen={setIsOpen}
+          url={data?.data?.memes[cellIndex]?.url}
+          id={data?.data?.memes[cellIndex]?.id}
+        />
+      )}
+    </>
   );
 }
